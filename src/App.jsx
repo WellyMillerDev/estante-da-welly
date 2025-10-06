fe28ee333dbac49ca38a1bfdd93f42b299976f8b
WellyMillerDev <wellymiller.dev@gmail.com>
2025-10-05 22:43:39 -0300

    BKP_05102025

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 import RoutesApp from './Routes'
 function App() {
 
   const [ISMOBILE, setISMOBILE] = useState(/iphone|ipad|ipod|android|windows phone/i.test(navigator.userAgent));
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, []);
 
   return (
     <>
